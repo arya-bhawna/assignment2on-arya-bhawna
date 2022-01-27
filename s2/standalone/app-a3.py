c09c4638fe7f92d9cff8c155aba7852a6fc8f64c
baa26 <bhawna.arya@mail.com>
2022-01-26 20:06:36 -0800

    Adding missing unique code to for a3 for submission

--- s2/standalone/app-a3.py
+++ s2/standalone/app-a3.py
@@ -121,8 +121,8 @@ def delete_song(music_id):
 
 @bp.route('/test', methods=['GET'])
 def test():
-    # This value is for user scp756-221
-    if ('d45538c9e14d4b18686a5a143f7fbee7ffb6cad84a5f916c3a5494f0e7980171' !=
+    # This value is for user scp756-221 #adding commit for unique code
+    if ('c2593b3d691fdd9db730a214469b2ccabd6e760e7ea8da24fdf6f339ab0b50fd' !=
             ucode):
         raise Exception("Test failed")
     return {}
