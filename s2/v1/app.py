19a9be84eb875e75c2be71232e909ace37f814f6
baa26 <bhawna.arya@mail.com>
2022-02-04 22:06:14 -0800

    Fixed app.py Line2

--- s2/v1/app.py
+++ s2/v1/app.py
@@ -127,7 +127,7 @@ def delete_song(music_id):
 @bp.route('/test', methods=['GET'])
 def test():
     # This value is for user scp756-221
-    if ('6cbd353eaadbc61c35132838888c136e96e31f10643fb2b472753b1acfb36e58' !=
+    if ('68669cf1fce773502073e526cf145450f4057f123d929e2007d74dd3fde0fc50' !=
             ucode):
         raise Exception("Test failed")
     return {}
